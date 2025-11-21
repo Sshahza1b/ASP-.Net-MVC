5268aadc0c22fcc398e5295636845ea055b57fa0
Shahzaib <129912416+Sshahza1b@users.noreply.github.com>
2025-11-21 17:15:42 +0500

    Add Migration

---
+++ MVC/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using MVC.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace MVC.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Category> Categories { get; set; }
+    }
+}
