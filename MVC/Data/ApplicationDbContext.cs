db1b1b251525752cdfccf2b6c36d3681e29c0885
Shahzaib <129912416+Sshahza1b@users.noreply.github.com>
2025-11-21 19:04:10 +0500

    add tables

--- MVC/Data/ApplicationDbContext.cs
+++ MVC/Data/ApplicationDbContext.cs
@@ -11,5 +11,15 @@ namespace MVC.Data
         }
 
         public DbSet<Category> Categories { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+           
+            modelBuilder.Entity<Category>().HasData(
+                new Category { Id = 1, Name = "Action", DisplayOrder = 1 },
+                new Category { Id = 2, Name = "Boil", DisplayOrder = 2 },
+                new Category { Id = 3, Name = "SciFi", DisplayOrder = 3 }
+            );
+        }
     }
 }
