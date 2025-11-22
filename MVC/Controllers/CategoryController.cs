d9f5a2d333a81a29ca6a740284ddcc6104ffaf58
Shahzaib <129912416+Sshahza1b@users.noreply.github.com>
2025-11-22 16:06:35 +0500

    Display Orders

--- MVC/Controllers/CategoryController.cs
+++ MVC/Controllers/CategoryController.cs
@@ -1,12 +1,20 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MVC.Data;
+using MVC.Models;
 
 namespace MVC.Controllers
 {
     public class CategoryController : Controller
     {
+        private readonly ApplicationDbContext _db;
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
         public IActionResult Index()
         {
-            return View();
+            List<Category> objCategoryList = _db.Categories.ToList();
+            return View(objCategoryList);
         }
     }
 }
