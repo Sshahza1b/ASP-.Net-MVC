37bd77918c207bebdc59310046fde1fbf733c571
Shahzaib <129912416+Sshahza1b@users.noreply.github.com>
2025-11-24 12:41:39 +0500

    create Category UI && make it responsive

--- MVC/Controllers/CategoryController.cs
+++ MVC/Controllers/CategoryController.cs
@@ -16,5 +16,10 @@ namespace MVC.Controllers
             List<Category> objCategoryList = _db.Categories.ToList();
             return View(objCategoryList);
         }
+
+        public IActionResult Create() 
+        {
+            return View();
+        }
     }
 }
