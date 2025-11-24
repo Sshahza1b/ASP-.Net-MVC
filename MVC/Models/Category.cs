ed02c30a3841f6998ede2b905c36f06c503e6e72
Shahzaib <129912416+Sshahza1b@users.noreply.github.com>
2025-11-24 14:54:09 +0500

    add alidations

--- MVC/Models/Category.cs
+++ MVC/Models/Category.cs
@@ -8,9 +8,11 @@ namespace MVC.Models
         [Key]
         public int Id { get; set; }
         [Required]
+        [MaxLength(20)]
         [DisplayName("Category Name")]
         public string Name { get; set; }
         [DisplayName("Display Order")]
+        [Range(1,50)]
         public int DisplayOrder { get; set; }
     }
 }
