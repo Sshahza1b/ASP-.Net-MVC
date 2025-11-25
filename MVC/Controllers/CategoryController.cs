136de2b1daf3ab2143105fd24d83b6de4f8a25ef
Shahzaib <129912416+Sshahza1b@users.noreply.github.com>
2025-11-25 17:15:19 +0500

    Add TemData

--- MVC/Controllers/CategoryController.cs
+++ MVC/Controllers/CategoryController.cs
@@ -33,6 +33,7 @@ namespace MVC.Controllers
             {
                 _db.Categories.Add(obj);
                 _db.SaveChanges();
+                TempData["success"] = "Category created successfully";
                 return RedirectToAction("Index");
             }
             
@@ -61,6 +62,7 @@ namespace MVC.Controllers
             {
                 _db.Categories.Update(obj);
                 _db.SaveChanges();
+                TempData["success"] = "Category updated successfully";
                 return RedirectToAction("Index");
             }
 
@@ -91,6 +93,7 @@ namespace MVC.Controllers
             }
             _db.Categories.Remove(obj);
             _db.SaveChanges();
+            TempData["success"] = "Category deleted successfully";
             return RedirectToAction("Index");
         }
     }
